c84ce1cb9a76cf02f6b25a4eb6d6a83a3846f7ea
magnitopic <magnitopic@gmail.com>
2021-09-02 20:12:33 +0200

    Added comments to the code

--- bot.py
+++ bot.py
@@ -4,19 +4,25 @@ from webServer import keep_alive
 
 #my_secret = os.environ['TOKEN']
 
-client=discord.Client()
+client = discord.Client()
+
 
 @client.event
 async def on_ready():
-	print("We have loged in as {0.user}".format(client))
+    print("We have loged in as {0.user}".format(client))
+
 
 @client.event
 async def on_message(message):
-	if message.author==client.user:
-		return
+    if message.author == client.user:
+        return
 
-	if message.content.startswith("$hello"):
-		await message.channel.send('Hello!')
+    if message.content.startswith("$hello"):
+        await message.channel.send('Hello!')
 
+# Calls the keep_alive function form webServer.py
 keep_alive()
+
+# When using .env file:
+# process.env.TOKEN
 client.run(os.environ['TOKEN'])
--- webServer.py
+++ webServer.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from threading import Thread
 
+"""
+This program creates a web server with flask witch
+is pinged every 5' so Replit keeps out bot alive
+"""
+
 app = Flask('')
+
 
 @app.route('/')
 def home():
     return "Hello. I am alive!"
 
+
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
+
 
 def keep_alive():
     t = Thread(target=run)
