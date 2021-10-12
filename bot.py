baad384f4e115401c43396a1154997b14106bc63
magnitopic <magnitopic@gmail.com>
2021-10-12 23:12:07 +0200

    New path for the bot

--- bot.py
+++ bot.py
@@ -1,16 +1,29 @@
 import discord
+import requests
 import os
-import rawgpy
+import json
+from dotenv import load_dotenv, find_dotenv
+from requests.api import get
 from webServer import keep_alive
 
 #my_secret = os.environ['TOKEN']
 
 client = discord.Client()
+load_dotenv(find_dotenv())
 
 
 @client.event
 async def on_ready():
     print("We have loged in as {0.user}".format(client))
+
+
+def get_apod():
+    # When using .env file:
+    # os.getenv('API_TOKEN')
+    resp = requests.get(
+        "https://api.nasa.gov/planetary/apod?api_key=" + os.environ['API_TOKEN'])
+    content = json.loads(resp.content)
+    #print(content)     Prints all the JSON
 
 
 @client.event
@@ -20,10 +33,12 @@ async def on_message(message):
 
     if message.content.startswith("$hello"):
         await message.channel.send('Hello!')
+    elif message.content.startswith("$apod") or message.content.startswith("$APOD"):
+        get_apod()
 
 # Calls the keep_alive function form webServer.py
 keep_alive()
 
 # When using .env file:
-# process.env.TOKEN
-client.run(os.environ['TOKEN'])
+# os.getenv('BOT_TOKEN')
+client.run(os.getenv('BOT_TOKEN'))  # os.environ['BOT_TOKEN'])
--- test.py
+++
@@ -1,5 +0,0 @@
-import rawgpy
-
-rawg = rawgpy.RAWG("bfc15713f1c546fe97d851001a34a478")
-results = rawg.search("Warframe")
-print(results)
